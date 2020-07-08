458ce5ddd51db1a30176e73a63b583cd708c44bb
Zhang, Xiaobing <xiaobing.zhang@intel.com>
2020-07-09 05:50:32 +0800

    BFloat16: enable prepacked weights's inference
    ghstack-source-id: 76bb2a8df275181801747fa2c08c69cbbf038bb9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37218

--- torch/utils/mkldnn.py
+++ torch/utils/mkldnn.py
@@ -4,9 +4,9 @@ import torch
 
 
 class MkldnnLinear(torch.jit.ScriptModule):
-    def __init__(self, dense_module):
+    def __init__(self, dense_module, dtype):
         super(MkldnnLinear, self).__init__()
-        self.register_buffer('weight', dense_module.weight.to_mkldnn())
+        self.register_buffer('weight', dense_module.weight.to_mkldnn(dtype))
         if dense_module.bias is not None:
             self.register_buffer('bias', dense_module.bias.to_mkldnn())
         else:
@@ -39,7 +39,6 @@ class _MkldnnConvNd(torch.jit.ScriptModule):
 
     def __init__(self, dense_module):
         super(_MkldnnConvNd, self).__init__()
-
         self.stride = dense_module.stride
         self.padding = dense_module.padding
         self.dilation = dense_module.dilation
@@ -83,11 +82,11 @@ class MkldnnConv1d(_MkldnnConvNd):
 
 
 class MkldnnConv2d(_MkldnnConvNd):
-    def __init__(self, dense_module):
+    def __init__(self, dense_module, dtype):
         super(MkldnnConv2d, self).__init__(dense_module)
 
         self.register_buffer('weight', torch._C._nn.mkldnn_reorder_conv2d_weight(
-            dense_module.weight.to_mkldnn(),
+            dense_module.weight.to_mkldnn(dtype),
             self.padding,
             self.stride,
             self.dilation,
@@ -157,23 +156,23 @@ class MkldnnBatchNorm2d(torch.jit.ScriptModule):
         )
 
 
-def to_mkldnn(module):
-    def m_fn(m):
+def to_mkldnn(module, dtype=torch.float):
+    def m_fn(m, d):
         if isinstance(m, torch.nn.Linear):
-            return MkldnnLinear(m)
+            return MkldnnLinear(m, d)
         elif isinstance(m, torch.nn.Conv1d):
             return MkldnnConv1d(m)
         elif isinstance(m, torch.nn.Conv2d):
-            return MkldnnConv2d(m)
+            return MkldnnConv2d(m, d)
         elif isinstance(m, torch.nn.BatchNorm2d):
             return MkldnnBatchNorm2d(m)
         else:
             return m
 
-    def m_fn_rec(m):
-        new_m = m_fn(m)
+    def m_fn_rec(m, d):
+        new_m = m_fn(m, d)
         for name, sub_m in m.named_children():
-            setattr(new_m, name, m_fn_rec(sub_m))
+            setattr(new_m, name, m_fn_rec(sub_m, d))
         return new_m
 
-    return m_fn_rec(module)
+    return m_fn_rec(module, dtype)
