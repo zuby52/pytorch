acb5c01cb6297bdabc5844c94bc6b3d4f815874d
Sinan Nasir <sinannasir@fb.com>
2020-07-11 18:54:26 -0700

    DDP Communication Hook Main Structure
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40848
    
    Sub-tasks 1 and 2 of [39272](https://github.com/pytorch/pytorch/issues/39272)
    ghstack-source-id: 107594678
    
    Differential Revision: [D22328310](https://our.internmc.facebook.com/intern/diff/D22328310/)

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -106,6 +106,28 @@ class PythonStore : public ::c10d::Store {
   }
 };
 
+// PythonhookBinder has a static function called register_comm_hook
+// that enables registering a c10d PythonCommHook object to c10d
+// reducer from Python.
+class PythonHookBinder {
+ public:
+  // This static method is called from DDP's Python API. Its inputs are
+  // a c10d reducer object, state, and callable comm_hook. State and
+  // comm_hook inputs are Python objects and this function creates a
+  // c10d PythonCommHook object using these inputs. It later calls
+  // register_comm_hook function of the reducer input to register that
+  // PythonCommHook object.
+  static void register_comm_hook(
+      py::object ddp_model,
+      py::object state,
+      py::object comm_hook) {
+    ddp_model.attr("reducer")
+        .cast<std::shared_ptr<::c10d::Reducer>>()
+        ->register_comm_hook(std::make_unique<::c10d::PythonCommHook>(
+            std::move(state), std::move(comm_hook)));
+  }
+};
+
 PyObject* c10d_init(PyObject* _unused) {
   C10_LOG_API_USAGE_ONCE("c10d.python.import");
   auto c10d_module = THPObjectPtr(PyImport_ImportModule("torch.distributed"));
@@ -114,6 +136,21 @@ PyObject* c10d_init(PyObject* _unused) {
   }
 
   auto module = py::handle(c10d_module).cast<py::module>();
+
+  shared_ptr_class_<::c10d::GradBucket>(module, "GradBucket")
+      .def(py::init<std::vector<Tensor>&>(), py::arg("tensors"))
+      .def(
+          "get_tensors",
+          &::c10d::GradBucket::getTensors,
+          py::call_guard<py::gil_scoped_release>());
+
+  shared_ptr_class_<PythonHookBinder>(module, "PythonHookBinder")
+      .def_static(
+          "register_comm_hook",
+          &PythonHookBinder::register_comm_hook,
+          py::arg("ddp_model"),
+          py::arg("state"),
+          py::arg("comm_hook"));
 
   shared_ptr_class_<::c10d::Reducer>(module, "Reducer")
       .def(
