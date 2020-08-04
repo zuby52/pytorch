31164f4dc3e4dd2b87e802a0e66c17f5cc769103
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-04 14:04:38 -0700

    Added add_scalar_
    ghstack-source-id: ff8906b8c18be52d20c2bdabc7e9716b261d0112
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42531

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -5,10 +5,21 @@ std::vector<Tensor> foreach_add_scalar_kernel_fallback(TensorList tensors, Scala
   TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
 
   std::vector<Tensor> result;
-  for (int i = 0; i < tensors.size(); i++) {
-    auto temp = tensors[i].add(scalar);
+  for (auto& t : tensors) {
+    auto temp = t.add(scalar);
     result.emplace_back(temp);
   }
   return result;
 }
+
+std::vector<Tensor> foreach_add_scalar__kernel_fallback(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  for (auto& t : tensors) {
+    t.add_(scalar);
+  }
+
+  return tensors.vec();
+}
+
 }} // namespace at::native
