2a179e0e9df73f5e278ca7c9b3c32b61f7a978f0
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-03 22:48:52 -0700

    Abstract GLSL source and SPIRV binary shaders in one common construct.
    ghstack-source-id: 905d2fb2f260560b44421b84772eb6e675103871
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42325

---
+++ aten/src/ATen/native/vulkan/api/Shader.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+#include <ATen/native/vulkan/api/Cache.h>
+#include <c10/util/hash.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+struct Shader final {
+  struct Descriptor final {
+    enum class Type {
+      Source,
+      Binary,
+    } type;
+
+    union {
+      struct {
+        const char* glsl; // Null-terminated
+        uint32_t unused;  // Padding
+      } source;
+
+      struct {
+        const uint32_t* spirv;
+        uint32_t size;    // Bytes
+      } binary;
+    } shader;
+
+    Descriptor(const char* glsl);
+    Descriptor(const uint32_t* spirv, uint32_t bytes);
+
+    inline bool operator==(const Descriptor& descriptor) const {
+      static_assert(
+          sizeof(descriptor.shader.source) == sizeof(descriptor.shader.binary),
+          "This implementation requires sizeof(Source) to be equal to sizeof(Binary).");
+
+      return (type == descriptor.type) &&
+             (shader.binary.spirv == descriptor.shader.binary.spirv) &&
+             (shader.binary.size == descriptor.shader.binary.size);
+    }
+  };
+
+  class Factory final {
+   public:
+    explicit Factory(VkDevice device);
+    Factory(const Factory&) = delete;
+    Factory& operator=(const Factory&) = delete;
+    Factory(Factory&&);
+    Factory& operator=(Factory&&);
+    ~Factory();
+
+    typedef Shader::Descriptor Descriptor;
+    typedef VK_DELETER(ShaderModule) Deleter;
+    typedef Handle<VkShaderModule, Deleter> Handle;
+
+    struct Hasher {
+      inline size_t operator()(const Descriptor& descriptor) const {
+        static_assert(
+            sizeof(descriptor.shader.source) == sizeof(descriptor.shader.binary),
+            "This implementation requires sizeof(Source) to be equal to sizeof(Binary).");
+
+        return c10::get_hash(
+            descriptor.type,
+            descriptor.shader.binary.spirv,
+            descriptor.shader.binary.size);
+      }
+    };
+
+    Handle operator()(const Descriptor& descriptor) const;
+
+   private:
+    VkDevice device_;
+    struct Compiler;
+    std::unique_ptr<Compiler> compiler_;
+  };
+
+  typedef api::Cache<Factory> Cache;
+};
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
